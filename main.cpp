4d6636f734b0fd1fc5f902c5ba2491eedc0b39d4
0000xFFFF <coldmailgg@gmail.com>
2024-09-20 06:05:21 +0200

    working utf8 functions

--- main.cpp
+++ main.cpp
@@ -13,6 +13,14 @@
 #include <unordered_set>
 #include <functional>
 #include <atomic>
+#include <cwchar>
+
+#include <string>
+#include <cstdlib>  // for free()
+#include <cctype>   // for std::tolower, std::toupper
+
+#include <locale>
+#include <codecvt>
 
 #include "UtfConv.h"
 
@@ -33,6 +41,92 @@ int min_len = 8;
 std::string input_file, output_file;
 std::ofstream output;
 
+size_t utf8_strlen(const std::string& str) {
+    size_t length = 0;
+    for (size_t i = 0; i < str.length(); ) {
+        unsigned char c = str[i];
+        if      (c <= 0x7F)              { i += 1;    } // 1-byte character (ASCII)
+        else if (c >= 0xC2 && c <= 0xDF) { i += 2;    } // 2-byte character
+        else if (c >= 0xE0 && c <= 0xEF) { i += 3;    } // 3-byte character
+        else if (c >= 0xF0 && c <= 0xF4) { i += 4;    } // 4-byte character
+        else                             { return -1; } // Invalid UTF-8 sequence encountered at byte i
+        ++length;
+    }
+    return length;
+}
+
+void utf8_reverse(char *str) {
+    /* this assumes that str is valid UTF-8 */
+    char *scanl, *scanr, *scanr2, c;
+
+    /* first reverse the string */
+    for (scanl= str, scanr= str + strlen(str); scanl < scanr;)
+        c= *scanl, *scanl++= *--scanr, *scanr= c;
+
+    /* then scan all bytes and reverse each multibyte character */
+    for (scanl= scanr= str; (c= *scanr++);) {
+        if ( (c & 0x80) == 0) // ASCII char
+            scanl= scanr;
+        else if ( (c & 0xc0) == 0xc0 ) { // start of multibyte
+            scanr2= scanr;
+            switch (scanr - scanl) {
+                case 4: c= *scanl, *scanl++= *--scanr, *scanr= c; // fallthrough
+                case 3: // fallthrough
+                case 2: c= *scanl, *scanl++= *--scanr, *scanr= c;
+            }
+            scanr= scanl= scanr2;
+        }
+    }
+}
+
+std::string utf8_tolower(const std::string& input) {
+    Utf16Char* utf16_input = Utf8ToUtf16(reinterpret_cast<const Utf8Char*>(input.c_str()));
+    if (!utf16_input) { return ""; }
+
+    Utf16Char* lower_utf16 = Utf16StrMakeLwrUtf16Str(utf16_input);
+    free(utf16_input);
+    if (!lower_utf16) { return ""; }
+
+    Utf8Char* lower_utf8 = Utf16ToUtf8(lower_utf16);
+    free(lower_utf16);
+    if (!lower_utf8) { return ""; }
+
+    std::string result(reinterpret_cast<char*>(lower_utf8));
+    free(lower_utf8);  // Free the UTF-8 string
+    return result;
+}
+
+std::string utf8_toupper(const std::string& input) {
+    Utf16Char* utf16_input = Utf8ToUtf16(reinterpret_cast<const Utf8Char*>(input.c_str()));
+    if (!utf16_input) { return ""; }
+
+    Utf16Char* upper_utf16 = Utf16StrMakeUprUtf16Str(utf16_input);
+    free(utf16_input);
+    if (!upper_utf16) { return ""; }
+
+    Utf8Char* upper_utf8 = Utf16ToUtf8(upper_utf16);
+    free(upper_utf16);
+    if (!upper_utf8) { return ""; }
+
+    std::string result(reinterpret_cast<char*>(upper_utf8));
+    free(upper_utf8);
+    return result;
+}
+
+std::string utf8_titlecase(const std::string& str) {
+
+    int i = 0;
+    unsigned char c = str[i];
+    size_t char_length = 0;
+    if      (c <= 0x7F)              { char_length = 1; } // 1-byte character (ASCII)
+    else if (c >= 0xC2 && c <= 0xDF) { char_length = 2; } // 2-byte character
+    else if (c >= 0xE0 && c <= 0xEF) { char_length = 3; } // 3-byte character
+    else if (c >= 0xF0 && c <= 0xF4) { char_length = 4; } // 4-byte character
+    else                             { return "";       } // Invalid UTF-8 sequence
+
+    return utf8_toupper(str.substr(i, char_length)) + str.substr(char_length);
+}
+
 void print_args() {
     std::cerr << "vlutr123cdzy\n" <<
     verbose << lower << upper << title << reverse << wordint << intword << intwordint << check << double_mode << double_small << year << std::endl;
@@ -40,37 +134,24 @@ void print_args() {
 
 void print_words() {
     std::cerr << "==[ BEGIN WORDSET ]==" << std::endl;
-    for (const auto& word : words) { std::cerr << word << std::endl; }
+    for (const auto& word : words) { std::cerr << word << " -- len: " << utf8_strlen(word) << std::endl; }
     std::cerr << "==[ END WORDSET ]==" << std::endl;
 }
 
 // Add variations of a word (lowercase, uppercase, title case, reverse)
 void add_word_variations(const std::string& word) {
     words.insert(word);
-    // TODO: FIX -- PROBLEMATIC FOR UTF8
-    if (lower) {
+    if (lower) { words.insert(utf8_tolower(word)); }
+    if (upper) { words.insert(utf8_toupper(word)); }
+    if (title) { words.insert(utf8_titlecase(word)); }
 
-
-
-
-    }
-    if (upper) {
-        char* buffer = (char*)calloc(sizeof(char), word.size());
-        strcpy(buffer, word.c_str());
-
-        Utf32Char* utf32chars = Utf8ToUtf32((Utf8Char*)buffer);
-        Utf32Char* utf32chars_upper = StrToUprUtf32(utf32chars);
-        words.insert(word);
-    }
-    if (title) {
-        std::string title_word = word;
-        title_word[0] = toupper(title_word[0]);
-        words.insert(title_word);
-    }
     if (reverse) {
-        std::string reversed_word = word;
-        std::reverse(reversed_word.begin(), reversed_word.end());
-        words.insert(reversed_word);
+        char* buff = new char[word.length()+1];
+        std::strcpy(buff, word.c_str());
+        utf8_reverse(buff);
+        std::string reversed(buff);
+        words.insert(reversed);
+        delete[] buff;
     }
 }
 
